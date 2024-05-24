bf7fd2424921396a3d6bee36d6211ffdf7d8c9df
NovikovRN <nightprofunkap@gmail.com>
2024-05-24 16:37:15 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   for_st.py

--- for_st.py
+++ for_st.py
@@ -19,7 +19,7 @@ if files:
     for_check =  full_dataframe.copy()
     for_check['check'] = for_check['block'] + ' : ' + for_check['quantity'].astype(str) + " шт."
     pivot_check = for_check.pivot_table(index='description', values=['check'], aggfunc=lambda x: ',<br>'.join(x)).reset_index()
-    html_text = "<br>".join(["<b>" + pivot_check.iloc[i, 0] + "</b> :<br>" + pivot_check.iloc[i, 1] + "<br>" for i in range(pivot_check.shape[0])])
+    html_text = "<br>".join(["<b>" + pivot_check.iloc[i, 0] + "</b> :<br>" + pivot_check.iloc[i, 1] + "<br>" for i in range(pivot_check.shape[0])]).replace("*", "\*")
     with st.expander("📋 Список всех блоков"):
         st.markdown(html_text, unsafe_allow_html=True)
     st.download_button(label='💾 Скачать файл для проверки', data=html_text, file_name='проверка.html')
