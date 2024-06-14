8b809da2ad5ac678f36f8e7c5fbd465d781d90ae
HeckfyVEZA <139766140+HeckfyVEZA@users.noreply.github.com>
2024-06-14 13:58:44 +0300

    Update help_defs.py

--- help_defs.py
+++ help_defs.py
@@ -6,21 +6,24 @@ from itertools import dropwhile, takewhile
 from data_freq import frequency_manager
 
 def sashQUA(main_system_name:str, the_mask = r'\d?[A-Za-zА-Яа-яЁё]\D?\D?\D?\D?\S?\S?\S?\S?\d{1,}[А-Яа-яЁё]?'):
-    all_system_names = []
-    for system_name in main_system_name.replace("-", " - ").split(','):
-        system_name_all = re.findall(the_mask, system_name.strip(), re.IGNORECASE)
-        system_name_all = system_name_all if system_name_all else [main_system_name]
-        if len(system_name_all) > 1:
-            all_positions_in_system_name = tuple(tuple(filter(None, re.findall(r'(\D?|\d+)', sys_name, re.IGNORECASE))) for sys_name in system_name_all)
-            system_condition = lambda x: x[0] == x[1]
-            before_changing_part = ''.join(el[0] for el in takewhile(system_condition, zip(all_positions_in_system_name[0], all_positions_in_system_name[1])))
-            after_changing_part = dropwhile(system_condition, zip(all_positions_in_system_name[0], all_positions_in_system_name[1]))
-            changing_part_start, changing_part_thend = next(after_changing_part)
-            after_changing_part = ''.join(el[0] for el in after_changing_part)
-            all_system_names += [f"{before_changing_part}{'0' * (min(len(changing_part_start), len(changing_part_thend)) - len(str(i)))}{i}{after_changing_part}" for i in range(int(changing_part_start), int(changing_part_thend) + 1)]
-        else:
-            all_system_names += system_name_all
-    return len(all_system_names)
+    try:
+        all_system_names = []
+        for system_name in main_system_name.replace("-", " - ").split(','):
+            system_name_all = re.findall(the_mask, system_name.strip(), re.IGNORECASE)
+            system_name_all = system_name_all if system_name_all else [main_system_name]
+            if len(system_name_all) > 1:
+                all_positions_in_system_name = tuple(tuple(filter(None, re.findall(r'(\D?|\d+)', sys_name, re.IGNORECASE))) for sys_name in system_name_all)
+                system_condition = lambda x: x[0] == x[1]
+                before_changing_part = ''.join(el[0] for el in takewhile(system_condition, zip(all_positions_in_system_name[0], all_positions_in_system_name[1])))
+                after_changing_part = dropwhile(system_condition, zip(all_positions_in_system_name[0], all_positions_in_system_name[1]))
+                changing_part_start, changing_part_thend = next(after_changing_part)
+                after_changing_part = ''.join(el[0] for el in after_changing_part)
+                all_system_names += [f"{before_changing_part}{'0' * (min(len(changing_part_start), len(changing_part_thend)) - len(str(i)))}{i}{after_changing_part}" for i in range(int(changing_part_start), int(changing_part_thend) + 1)]
+            else:
+                all_system_names += system_name_all
+        return len(all_system_names)
+    except:
+        return 1
 
 def information_from_file(uploaded_file):
     canal_dataframe = pd.DataFrame()
